30bf7f3bfb54c900b7f0f9019c7c0b3f47b06c78
AstraidLabs <46496187+AstraidLabs@users.noreply.github.com>
2025-12-27 23:18:22 +0100

    Rename static playlist parse helper

--- M3uEditor.Core.Tests/PlaylistTests.cs
+++ M3uEditor.Core.Tests/PlaylistTests.cs
@@ -13,7 +13,7 @@ public class PlaylistTests
     public void RoundtripPlainPlaylistPreservesContent()
     {
         var content = "track1.mp3\ntrack2.mp3";
-        var document = PlaylistParser.Parse(content);
+        var document = PlaylistParser.ParseText(content);
 
         document.DetectedKind.Should().Be(PlaylistKind.PlainM3u);
         PlaylistWriter.Write(document).Should().Be(content);
@@ -23,7 +23,7 @@ public class PlaylistTests
     public void RoundtripExtendedPlaylistPreservesCrLf()
     {
         var content = "#EXTM3U\r\n#EXTINF:-1 tvg-id=\"id1\",Channel 1\r\nhttp://example.com/1\r\n";
-        var document = PlaylistParser.Parse(content);
+        var document = PlaylistParser.ParseText(content);
 
         document.NewLine.Should().Be("\r\n");
         PlaylistWriter.Write(document).Should().Be(content);
@@ -33,7 +33,7 @@ public class PlaylistTests
     public void RoundtripHlsMasterPreservesTags()
     {
         var content = "#EXTM3U\n#EXT-X-VERSION:6\n#EXT-X-STREAM-INF:BANDWIDTH=1280000,RESOLUTION=1280x720\nmaster1.m3u8\n#EXT-X-STREAM-INF:BANDWIDTH=640000,RESOLUTION=854x480\nmaster2.m3u8";
-        var document = PlaylistParser.Parse(content);
+        var document = PlaylistParser.ParseText(content);
 
         document.DetectedKind.Should().Be(PlaylistKind.HlsMaster);
         PlaylistWriter.Write(document).Should().Be(content);
@@ -43,7 +43,7 @@ public class PlaylistTests
     public void RoundtripHlsMediaPreservesUnknownTags()
     {
         var content = "#EXTM3U\n#EXT-X-VERSION:3\n#EXT-X-TARGETDURATION:6\n#EXT-X-MEDIA-SEQUENCE:1\n#EXTINF:6.0,First\nsegment1.ts\n#EXT-UNKNOWN:demo\n#EXTINF:6.0,Second\nsegment2.ts";
-        var document = PlaylistParser.Parse(content);
+        var document = PlaylistParser.ParseText(content);
 
         document.DetectedKind.Should().Be(PlaylistKind.HlsMedia);
         PlaylistWriter.Write(document).Should().Be(content);
@@ -54,7 +54,7 @@ public class PlaylistTests
     public void ReplaceAllUpdatesOnlyMatchingLines()
     {
         var content = "alpha\nbeta\nalpha";
-        var document = PlaylistParser.Parse(content);
+        var document = PlaylistParser.ParseText(content);
         var options = new FindReplaceOptions { FindText = "alpha", ReplaceText = "gamma", MatchCase = true, WholeWord = false, UseRegex = false };
 
         var count = FindReplaceService.ReplaceAll(document, options);
@@ -66,7 +66,7 @@ public class PlaylistTests
     public void DiagnosticsProvideCorrectSpan()
     {
         var content = "#EXTINF:9,Missing uri";
-        var document = PlaylistParser.Parse(content);
+        var document = PlaylistParser.ParseText(content);
 
         document.Diagnostics.Should().ContainSingle(d =>
             d.Code == "TAG001" &&
